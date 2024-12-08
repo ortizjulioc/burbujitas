e5480fe9e0dd6579cf0bd613ac5aedbb125c5976
ortizjulioc <ortizjuliocesar101@gmail.com>
2024-12-07 21:19:30 -0400

    arreglo de la explocion de las burbujas

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
-const burbujas = document.querySelectorAll('.burbujas div, .burbujas2 div')
+const burbujas = document.querySelectorAll('.burbujas div, .burbujas2 div');
 
-
-
-function burbujaClick(event){
-    const sonido = document.getElementById('popSound')
+async function burbujaClick(event) {
+    const sonido = new Audio('./pop.mp3'); // Cambia a la ruta correcta de tu archivo de audio
     sonido.play();
 
     const burbuja = event.target;
-    burbuja.style.opacity = '0'
+    burbuja.style.opacity = '0';
     setTimeout(() => {
         burbuja.style.display = 'none';
-
-    }, 200)
-
+    }, 200);
 }
 
 burbujas.forEach(burbuja => {
-    burbuja.addEventListener('click',burbujaClick)
+    burbuja.addEventListener('click', burbujaClick);
 });
